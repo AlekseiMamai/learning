5e3dfce7474f499aecac35ebf9e2d5c42f087de4
Alex <lesha.mamai228@gmail.com>
2023-06-17 21:24:40 +0300

    Methods

--- 13/script.js
+++ 13/script.js
@@ -1,2 +1,71 @@
 "use stcrict";
 
+// function showFirstMessege(text) {
+//     console.log(text);
+// }
+
+// showFirstMessege("Hello World!");
+
+let num = 20; //доступна глобально
+
+function showFirstMessege(text) {
+    console.log(text);
+    //let num = 10;      // не доступна глобально
+    num = 10;
+}
+
+showFirstMessege("Hello World!");
+console.log(num);
+
+
+
+
+
+function calc(a, b){
+    return(a+b); //после return ничего не происходит, 
+                // на нём функция заканчивается 
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+
+
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+//классификация функций
+
+//1) функция выше - работает как var, даже если
+// вызывается перед её созданием
+
+//2)
+
+const logger = function() {
+    console.log('Hello!');
+}; // ставим запятую
+
+logger();
+
+//эта функция не может быть вызвана до её создания
+
+
+//3)
+
+const calc = (a, b) => a + b;
+const calc = (a, b) => {
+    return a + b;
+}; // также ставится запятая
+
+calc(4, 5);
+
+// нельзя использовать везде
--- 14/script.js
+++ 14/script.js
@@ -1,71 +1,33 @@
 "use stcrict";
 
-// function showFirstMessege(text) {
-//     console.log(text);
-// }
+const str = 'test'; //длина строки
 
-// showFirstMessege("Hello World!");
+console.log(str.length);
 
-let num = 20; //доступна глобально
 
-function showFirstMessege(text) {
-    console.log(text);
-    //let num = 10;      // не доступна глобально
-    num = 10;
-}
 
-showFirstMessege("Hello World!");
-console.log(num);
+const arr = [1, 2, 4]; // длина массива
 
+console.log(arr.length);
+
+
+
+console.log(str[2]); //выводит символ
+
+console.log(str.toUpperCase()); //повышает регистр, но не изменяет исходные данные
 
 
 
 
-function calc(a, b){
-    return(a+b); //после return ничего не происходит, 
-                // на нём функция заканчивается 
-}
+const fruit = 'some fruit';
 
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
+console.log(fruit.indexOf('fruit')); // нахождение искомого значения
 
+//модификация неизменяемой строки
+//1) метод слайс
+const logg = 'Hello World';
 
+console.log(logg.slice(0,6)); //последний аргумент не включает символы
+console.log(logg.slice(6)); //отсеивание всё позади 6 позиции
+//этот метод поддерживает отрицательные значения
 
-
-
-function ret() {
-    let num = 50;
-    return num;
-}
-
-const anotherNum = ret();
-console.log(anotherNum);
-
-
-//классификация функций
-
-//1) функция выше - работает как var, даже если
-// вызывается перед её созданием
-
-//2)
-
-const logger = function() {
-    console.log('Hello!');
-}; // ставим запятую
-
-logger();
-
-//эта функция не может быть вызвана до её создания
-
-
-//3)
-
-const calc = (a, b) => a + b;
-const calc = (a, b) => {
-    return a + b
-}; // также ставится запятая
-
-calc(4, 5);
-
-// нельзя использовать везде
