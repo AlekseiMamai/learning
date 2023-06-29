e7ad32da19a6ec5101f77a75c6dfaeb4ac4d2a27
Alex <lesha.mamai228@gmail.com>
2023-06-29 16:50:57 +0300

    obj metods

--- 17/script.js
+++ 17/script.js
@@ -1,38 +1,84 @@
 "use stcrict";
 
-function first() {
- //
- setTimeout(function(){
-    console.log(1);
- }, 500);
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     }
+    
+// };
+
+console.log(options.name);
+
+delete options.name; //удаление компонентов объекта
+
+console.log(options);
+
+
+
+
+// for (let key in options) { // конструкция for key 
+//                             //для перебора ключей объекта
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`)
+// }
+
+//мы выводим содержимое объекта в виде строки
+//объект внутри объекта он выведет в виде object Object
+//можно создать перебор для объектов внутри объекта с помощью
+//условий
+
+for (let key in options) { 
+    if (typeof(options[key]) === 'object'){
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`)
+        }
+    } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`)
+    }
 }
 
-function second () {
-    console.log(2);
-}
-
-first();
-second(); //функция 2 срабатывает быстрее, чем 1 из-за задержки
-// какой бы она ни была
 
 
-function learnJS (lang, callback) { // callback это аргумент 
-//другой функции
+//функции и методы объекта
 
-    console.log(`Я учу ${lang}`);
-    callback();
-}
 
-function done () {
-    console.log('Я прошёл этот урок');
-}
+console.log(Object.keys(options).length);
 
-learnJS('JavaScript', done);
+//чтобы не создавать перебор для нахождения количества
+//ключей, можно использовать метод объектов Object.keys
+//он позволяет обращаться к объекту как к строке и использовать его
+//свойства, например, length
+//Так находится количество ключей методом объекта
 
-//функцию можно вызывать анонимно:
-//learnJS('JavaScript', function (){
-    //console.log('Я прошёл этот урок');
-//});
 
-//Но лучше создавать функцию заранее и вызывать её в аргументе 
-//только названием: learnJS('JavaScript', done);
+
+//можно создавать методы самостоятельно в виде функций
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+console.log(Object.keys(options).length);
+
+options.makeTest();
+
+
+
+//деструктуризация объектов
+
+const {border, bg} = options.colors; //вытаскиваем 
+//свойства объектов для отдельной работы с ними
+
+console.log(border);
