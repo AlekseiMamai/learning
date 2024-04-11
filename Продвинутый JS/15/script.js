fe663eab7422ee5648c71f1e5b1d03895cc37a96
Alex <lesha.mamai228@gmail.com>
2024-04-11 22:24:44 +0300

    add folder 3.15 local storage

---
+++ Продвинутый JS/15/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//LocalStorage
+
+// localStorage.setItem('number', 5);
+
+// localStorage.removeItem('number');
+
+// localStorage.clear();
+
+// console.log(localStorage.getItem('number'));
+
+const checkbox = document.querySelector('#checkbox'),
+      form = document.querySelector('form'),
+      change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+    checkbox.checked = true;
+}
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('isChecked', true);
+});
+
+if (localStorage.getItem('bg') === 'changed') {
+    form.style.backgroundColor = 'red';
+}
+
+change.addEventListener('click', () => {
+    if (localStorage.getItem('bg') === 'changed') {
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = 'white';
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = 'red';
+    }
+});
+
+const persone = {
+    name: 'Alex',
+    age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+localStorage.setItem('alex', serializedPersone);
+
+console.log(JSON.parse(localStorage.getItem('alex')));
+
+// localStorage.setItem('alex', persone);
+// console.log((localStorage.getItem('alex')));
