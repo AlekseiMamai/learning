65b521c750c0687b316f14f482e446fbd9db9561
Alex <lesha.mamai228@gmail.com>
2024-05-01 16:49:03 +0300

    4.11 node-sass, reactstrap, styled-components

--- Библиотека React. Базовый уровень/6/my-app/src/components/app-header/app-header.js
+++ Библиотека React. Базовый уровень/6/my-app/src/components/app-header/app-header.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+// import './app-header.css';
 
-import './app-header.css';
+const Header = styled.div`
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    :hover {
+        color: gray;
+    }
+    h1 {
+        font-size: 26px;
+        color: ${props => props.colored ? 'red' : 'black'}
+        //Условие в стилях
+    }
+
+    h2 {
+        font-size: 1.2rem;
+        color: grey;
+    }
+`;
 
 const AppHeader = () => {
     return (
-        <div className="app-header d-flex">
+        // <Header colored>
+        //     <h1>Aleksei Mamai</h1>
+        //     <h2>5 записей, из них понравилось 0</h2>
+        // </Header>
+
+        // <Header as='a'> использование как ссылка
+        //     <h1>Aleksei Mamai</h1>
+        //     <h2>5 записей, из них понравилось 0</h2>
+        // </Header>
+        
+        <Header >
             <h1>Aleksei Mamai</h1>
             <h2>5 записей, из них понравилось 0</h2>
-        </div>
+        </Header>  
     )
 }
 
--- Библиотека React. Базовый уровень/6/my-app/src/components/app/app.js
+++ Библиотека React. Базовый уровень/6/my-app/src/components/app/app.js
@@ -7,7 +7,17 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
 
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
+
+//AppBlock можно использовать повторно с изменениями:
+// const StyledAppBlock = styled(AppBlock)`
+//     background-color: grey;
+// `;
 
 const App = () => {
 
@@ -18,7 +28,7 @@ const App = () => {
     ];
 
         return (
-            <div className="app">
+            <AppBlock>
                 <AppHeader/>
                 <div className="search-panel d-flex">
                     <SearchPanel/>
@@ -26,8 +36,22 @@ const App = () => {
                 </div>
                 <PostList posts = {data}/>
                 <PostAddForm/>
-            </div>
+            </AppBlock>
     )
+
+
+    // Использование новой копии компонента стилей StyleAddBlock
+    // return (
+    //     <StyledAppBlock>
+    //         <AppHeader/>
+    //         <div className="search-panel d-flex">
+    //             <SearchPanel/>
+    //             <PostStatusFilter/>
+    //         </div>
+    //         <PostList posts = {data}/>
+    //         <PostAddForm/>
+    //     </StyledAppBlock>
+    // )
 }
 
 export default App;
