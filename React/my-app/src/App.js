f833d0ea585eaf675af49023640afcea1d886635
Alex <lesha.mamai228@gmail.com>
2024-05-10 22:30:46 +0300

    pagination last point

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -9,6 +9,7 @@ import './styles/app.css'
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
 import { useFetching } from './hooks/useFetching';
+import { getPageCount, getPagesArray } from './utils/pages';
 
 function App() {
 
@@ -16,16 +17,22 @@ function App() {
   // const [title, setTitle] = useState('');//1)двухстороннее связывание для 1 способа
   const [filter, setFilter] = useState({sort: '', query: ''});
   const [modal, setModal] = useState(false);
-  const [totalCount, setTotalCount] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(1);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  
+  let pagesArray = getPagesArray(totalPages);
+
+  console.log(pagesArray);
+
   const [fetchPosts, isPostsLoading, postError] = useFetching( async () => {
-    const response = await PostService.getAll();
+    const response = await PostService.getAll(limit, page);
     setPosts(response.data);
-    console.log(response.headers['x-total-count'])
-    setTotalCount(response.headers['x-total-count']);
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit));
   });
 
+  console.log(totalPages);
   useEffect(() => {
     fetchPosts();
   },[])
@@ -38,6 +45,11 @@ function App() {
   //получаем пост из дочернего компонента для удаления
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const changePage = (page) => {
+    setPage(page);
+    fetchPosts();
   }
 
   return (
@@ -59,6 +71,16 @@ function App() {
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
       }
+      <div className="page__wrapper">
+      {pagesArray.map(p => 
+        <span
+          onClick={() => changePage(p)} 
+          key={p} 
+          className={page === p ? 'page page__current' : 'page'}>
+            {p}
+        </span>
+      )}
+      </div>
     </div>
   );
 }
