9109a8835438b245661c4024cf1958b867e3f9f4
Alex <lesha.mamai228@gmail.com>
2024-05-12 19:17:11 +0300

    Login last point

--- React/my-app/src/components/AppRouter.jsx
+++ React/my-app/src/components/AppRouter.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import About from '../pages/About';
-import Posts from '../pages/Posts';
-import Error from '../pages/Error';
+import { privateRoutes, publicRoutes } from '../router';
+import { AuthContext } from '../context';
+import Loader from './UI/Loader/Loader';
 
 const AppRouter = () => {
+    const {isAuth, isLoading} = useContext(AuthContext);
+
+    if(isLoading) {
+      return <Loader/>
+    }
+
     return (
+      isAuth 
+        ? 
         <Routes>
-          <Route path='/about' element={<About />}/>
-          <Route path='/posts' element={<Posts />}/>
-          <Route path='/error' element={<Error />}/>
-          <Route path="*" element={<Navigate to='/error' />}/>
+        {privateRoutes.map(route => 
+          <Route 
+            path={route.path} 
+            element={route.element}
+            exact={route.exact}
+            key={route.path}/>
+        )}
+        <Route path="*" element={<Navigate to='/posts' />}/>
+      </Routes>
+        : 
+        <Routes>
+          {publicRoutes.map(route => 
+            <Route 
+              path={route.path} 
+              element={route.element}
+              exact={route.exact}
+              key={route.path}/>
+          )}
+          <Route path="*" element={<Navigate to='/login' />}/>
         </Routes>
+        
     );
 };
 
